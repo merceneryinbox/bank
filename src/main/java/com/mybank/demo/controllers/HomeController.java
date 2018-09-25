b4addd432280512ce951bb9411184c18d082744e
mercenery@inbox.ru <mggk3511>
2018-09-25 10:49:30 +0300

    web

--- src/main/java/com/mybank/demo/controllers/HomeController.java
+++ src/main/java/com/mybank/demo/controllers/HomeController.java
@@ -3,16 +3,19 @@ package com.mybank.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "redirect:/index.html";
+        return "redirect:/index";
     }
 
     @RequestMapping("/index")
-    public String index() {
-        return "redirect:/index.html";
+    public String index(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setHeader("Cache-control", "no-cache,no-store,must-revalidate asdf");
+        return "/Login_v2/Login_v2/index";
     }
 }
